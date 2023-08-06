2a557dec1da6a66c395c3fd65dc595ca3f5d42ff
ubombar <ufukbombar@gmail.com>
2023-08-06 12:17:55 +0200

    Add example pdf

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+from PyPDF2 import PdfReader
 
-if __name__ == "__main__":
-    print("test")
+reader = PdfReader("example.pdf")
+
+for page in reader.pages:
+    if "/Annots" in page:
+        for annot in page["/Annots"]:
+            print(annot)
